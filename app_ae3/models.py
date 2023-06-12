5d56da519cd975333465579553d4771c5a468f7f
vverac <viverace@gmail.com>
2023-06-12 08:01:48 -0400

    primer commit trabInd ae3

---
+++ app_ae3/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+# from django.conf import settings
+
+# from django.utils import timezone
+
+
+class Cliente(models.Model):
+  # def __init__(self, nombre, edad):
+      nombre = models.CharField(max_length=200)
+      edad = models.CharField(max_length=200)
+   
+    # def publish(self):
+    #  pass
+
+      def __str__(self):
+        return self.nombre
